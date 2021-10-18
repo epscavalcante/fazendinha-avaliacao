fb8a6a9cfc29df19df296459096085a21db2dc45
Eduardo Cavalcante <epscavalcant@gmail.com>
2021-10-18 14:47:57 -0400

    add api resources

---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return array_merge(parent::toArray($request), [
+            'lauches' => LauchResource::collection($this->whenLoaded('lauches')),
+            'events' => EventResource::collection($this->whenLoaded('events'))
+        ]) ;
+    }
+}
