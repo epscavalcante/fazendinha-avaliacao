cc8a80cecef80e09d1c1e4d1645ab7a499677df1
Eduardo Cavalcante <epscavalcant@gmail.com>
2021-10-18 22:00:23 -0400

    class uuid, e add fillable

--- app/Models/Launch.php
+++ app/Models/Launch.php
@@ -6,7 +6,7 @@ use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Event extends Model
+class Launch extends Model
 {
     use HasFactory;
 
